f4b2cc10adf8d0de4c8f8f9512149b0c0e1f8175
WinterJackson <winterjacksonwj@gmail.com>
2023-09-22 09:26:25 +0300

    Completed Assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,19 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        try:
+            # Decode the bytes to a string and then parse it as JSON
+            data = json.loads(response_body.decode('utf-8'))
+            return data
+        except json.JSONDecodeError:
+            # Handle the case where the response is not valid JSON
+            return None
